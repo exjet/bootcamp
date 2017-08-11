21bfbb288624c4b47edec284da2f5c5a5a08f5a5
Jason Thibodeau <jason.thibodeau@cvscaremark.com>
2017-08-10 23:12:22 -0400

    Fixing entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+root@bootcamp-exercise-jason:~/exercise# vi ./entrypoint.sh 
+
 #!/bin/bash
 set -e
 # This entrypoint is used to play nicely with docker-compose which relies
@@ -12,8 +14,10 @@ if [ -z "$POSTGRES_ENV_POSTGRES_USER" ]; then
     export POSTGRES_ENV_POSTGRES_USER=postgres
 fi
 
-export DATABASE_URL=postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@postgres:5432/$POSTGRES_ENV_POSTGRES_USER
+export DATABASE_URL=postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@dbcontainer:5432/bootcamp
 
 export CELERY_BROKER_URL=$DJANGO_CACHE_URL
+
+python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
